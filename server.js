8ea42b481bf0a95eb073c7d1d5060b3dcdc69cd6
Oluwafemi Amballi <148333689+amberkartel@users.noreply.github.com>
2026-04-12 20:29:58 +0100

    Update server.js

--- server.js
+++ server.js
@@ -98,4 +98,7 @@ const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
     console.log("Server running on port " + PORT);
+    console.log("TOKEN RAW:", BOT_TOKEN);
+    console.log("TOKEN LENGTH:", BOT_TOKEN?.length);
+    
 });
